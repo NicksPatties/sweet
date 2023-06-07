9ea7c66ddcfdeb93adc277ce5af00ae913de1d94
NicksPatties <nickspatties@gmail.com>
2023-06-07 09:14:44 -0700

    Remove exercises directory

--- exercises/py/hello.py
+++
@@ -1,5 +0,0 @@
-def sayHello():
-    print('Hello, World!')
-
-if __name__ == '__main__':
-    sayHello()
--- exercises/sql/hello.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE helloworld (phrase TEXT);
-INSERT INTO helloworld VALUES ("Hello, World!");
-INSERT INTO helloworld VALUES ("Goodbye, World!");
-SELECT COUNT(*) FROM helloworld;
