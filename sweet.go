38a98f3ceb9be8294b702f3ea61c8c3f959e5ef5
NicksPatties <nickspatties@gmail.com>
2023-06-08 17:18:59 -0700

    Update README. Update documentation in sweet command. Update gitignore.

--- sweet.go
+++ sweet.go
@@ -1,17 +1,4 @@
 /*
-Hey! That's
-
-	      ,gg,                                                  gg
-	     i8""8i                                        I8      ,gg,
-	     `8,,8'                                        I8      i88i
-	      `88'                                      88888888   i88i
-	      dP"8,                                        I8      i88i
-	     dP' `8a  gg    gg    gg    ,ggg,    ,ggg,     I8      ,gg,
-	    dP'   `Yb I8    I8    88bg i8" "8i  i8" "8i    I8       gg
-	_ ,dP'     I8 I8    I8    8I   I8, ,8I  I8, ,8I   ,I8,
-	"888,,____,dP,d8,  ,d8,  ,8I   `YbadP'  `YbadP'  ,d88b,     aa
-	a8P"Y88888P" P""Y88P""Y88P"   888P"Y888888P"Y88888P""Y88    88
-
 Sweet is a Software Engineering Exercise for Typing. In other words, it's a touch typing
 exercise command line interface specifically designed for programmers.
 
