c30c68f85c3638e1e1909bc86bc0be897330ad18
NicksPatties <nickspatties@proton.me>
2024-10-04 17:38:16 -0700

    Add version compiler variable to version command
    - Write test for version command
    - Update release script to add version variable to about and version command

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -13,12 +13,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const CommandName = "version"
+// Included via a compiler flag.
+// i.e. go build -ldflags "-X github.com/NicksPatties/sweet/cmd/version.version=v0.1.0" .
+var version string
 
 var Command = &cobra.Command{
 	Use:   "version",
 	Short: "prints the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("version")
+		fmt.Print(getVersion(version))
 	},
+}
+
+func getVersion(v string) string {
+	if v == "" {
+		return "dev"
+	}
+	return v
 }
