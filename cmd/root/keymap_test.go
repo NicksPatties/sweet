7341f8a2e2b9a23b4a64eaffdec22f4b88dddb3f
NicksPatties <nickspatties@proton.me>
2024-11-06 13:22:01 -0800

    Add fingerView function. Creates a basic finger map.

--- cmd/root/keymap_test.go
+++ cmd/root/keymap_test.go
@@ -1,6 +1,84 @@
 package root
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
+
+const (
+	lpinky uint = iota
+	lring
+	lmiddle
+	lindex
+	lthumb
+	rthumb
+	rindex
+	rmiddle
+	rring
+	rpinky
+)
+
+var fm map[string][]int = make(map[string][]int)
+
+// func charToFingers() []int {
+
+// }
+
+func fingerView() (view string) {
+	c := "*"
+	// fingers
+	f := [][]string{
+		{"0", c},
+		{"1", c, c},
+		{"2", c, c},
+		{"3", c},
+		{"4"},
+		{"5"},
+		{"6", c},
+		{"7", c, c},
+		{"8", c, c},
+		{"9", c},
+	}
+
+	for row := 2; row >= 0; row-- {
+		// curr fingers
+		for cf := 0; cf < len(f); cf++ {
+			// if this is a finger spot, then I should print the character
+			// in the finger view location
+			if isFingerSpot := row < len(f[cf]); isFingerSpot {
+				// label?
+				if row == 0 {
+					view += strconv.Itoa(cf)
+				} else {
+					view += c
+				}
+			} else {
+				view += " "
+			}
+			// space in between the hands
+			if cf == 4 {
+				view += " "
+			}
+		}
+		// not last row
+		if row != 0 {
+			view += "\n"
+		}
+	}
+	return
+}
+
+func TestFingerView(t *testing.T) {
+	name := "No highlighted characters, stars for fingers"
+	want := "" +
+		" **     ** \n" +
+		"****   ****\n" +
+		"01234 56789"
+	got := fingerView()
+	if got != want {
+		t.Errorf("%s:\ngot:\n%s\n\nwant:\n%s", name, got, want)
+	}
+}
 
 func TestFindKeyCombo(t *testing.T) {
 	testCases := []struct {
