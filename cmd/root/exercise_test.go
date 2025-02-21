a80522dacd9da17cee766bda61dca1267134c4de
NicksPatties <nickspatties@proton.me>
2025-02-20 20:08:47 -0800

    Add newline character test for renderText function

--- cmd/root/exercise_test.go
+++ cmd/root/exercise_test.go
@@ -25,23 +25,47 @@ func TestRenderName(t *testing.T) {
 }
 
 func TestRenderText(t *testing.T) {
-	// TODO: if this function is indented with tabs, then this test fails
 	testText := `func main() {
     fmt.Println("hello!")
 }
 `
-	testModel := exerciseModel{
-		name:      "",
-		text:      testText,
-		typedText: "",
-		startTime: time.Time{},
-		endTime:   time.Time{},
-		quitEarly: false,
-		events:    []event.Event{},
+	tt := []struct {
+		name  string
+		text  string
+		typed string
+		want  string
+	}{
+		{
+			name:  "default test",
+			text:  testText,
+			typed: "",
+			want:  testText,
+		},
+		{
+			name:  "add newline character at end of line",
+			text:  testText,
+			typed: "func main() {",
+			want: `func main() {↲
+    fmt.Println("hello!")
+}
+`},
 	}
-	want := testText
-	got := testModel.renderText()
-	if got != want {
-		t.Fatalf("expected\n%s\ngot\n%s", want, got)
+
+	for _, test := range tt {
+		// TODO: if this function is indented with tabs, then this test fails
+		testModel := exerciseModel{
+			name:      "",
+			text:      test.text,
+			typedText: test.typed,
+			startTime: time.Time{},
+			endTime:   time.Time{},
+			quitEarly: false,
+			events:    []event.Event{},
+		}
+		got := testModel.renderText()
+		if got != test.want {
+			t.Fatalf("expected\n%s\ngot\n%s", test.want, got)
+		}
+
 	}
 }
